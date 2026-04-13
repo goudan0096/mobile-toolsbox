178762dc931a7364e21256ca9c0226c7c81eb68a
goudan0096 <tanjie0096@163.com>
2026-04-13 16:03:09 +0800

    没有编译报错

---
+++ ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mobile-toolbox
+//
+//  Created by 战斗菜鸡 on 2026/4/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
