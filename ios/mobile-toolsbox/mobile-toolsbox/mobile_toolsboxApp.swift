52511537cfdcf930e3635748bcf135889146434d
goudan0096 <tanjie0096@163.com>
2026-04-13 14:46:47 +0800

    空的一个ios应用项目

---
+++ ios/mobile-toolsbox/mobile-toolsbox/mobile_toolsboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_toolsboxApp.swift
+//  mobile-toolsbox
+//
+//  Created by 战斗菜鸡 on 2026/4/13.
+//
+
+import SwiftUI
+
+@main
+struct mobile_toolsboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
